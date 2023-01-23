4333ee39c6d4d628de885e3bebf6a7964fc3ba84
programarivm <info@programarivm.com>
2023-01-24 00:39:06 +0100

    Added validate.php

---
+++ validate.php
@@ -0,0 +1,49 @@
+<?php
+
+include __DIR__ . '/vendor/autoload.php';
+
+use Rubix\ML\Loggers\Screen;
+use Rubix\ML\Datasets\Labeled;
+use Rubix\ML\PersistentModel;
+use Rubix\ML\Persisters\Filesystem;
+use Rubix\ML\CrossValidation\Reports\AggregateReport;
+use Rubix\ML\CrossValidation\Reports\ConfusionMatrix;
+use Rubix\ML\CrossValidation\Reports\MulticlassBreakdown;
+
+ini_set('memory_limit', '-1');
+
+$logger = new Screen();
+
+$logger->info('Loading data into memory');
+
+$samples = $labels = [];
+
+$pieces = [ 'B', 'b', 'K', 'k', 'N', 'n', 'P', 'p', 'Q', 'q', 'R', 'r', ];
+
+foreach ($pieces as $piece) {
+    foreach (glob("training/$piece/*.jpg") as $file) {
+        $samples[] = [imagecreatefromjpeg($file)];
+        $labels[] = $piece;
+    }
+}
+
+$dataset = new Labeled($samples, $labels);
+
+$estimator = PersistentModel::load(new Filesystem('piece.rbx'));
+
+$logger->info('Making predictions');
+
+$predictions = $estimator->predict($dataset);
+
+$report = new AggregateReport([
+    'breakdown' => new MulticlassBreakdown(),
+    'matrix' => new ConfusionMatrix(),
+]);
+
+$results = $report->generate($predictions, $dataset->labels());
+
+echo $results;
+
+$results->toJSON()->saveTo(new Filesystem('report.json'));
+
+$logger->info('Report saved to report.json');
