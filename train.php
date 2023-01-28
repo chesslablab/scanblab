c07d9313959106e06ba7d4beb9d672337488e687
programarivm <info@programarivm.com>
2023-01-28 17:10:42 +0100

    Replaced piece with folder

--- train.php
+++ train.php
@@ -26,12 +26,12 @@ $logger->info('Loading data into memory');
 
 $samples = $labels = [];
 
-$pieces = [ 'B', 'b', 'K', 'k', 'N', 'n', 'P', 'p', 'Q', 'q', 'R', 'r', ];
+$folders = [ 'B', 'b', 'K', 'k', 'N', 'n', 'P', 'p', 'Q', 'q', 'R', 'r', 'empty', ];
 
-foreach ($pieces as $piece) {
-    foreach (glob("training/$piece/*.jpg") as $file) {
+foreach ($folders as $folder) {
+    foreach (glob("training/$folder/*.jpg") as $file) {
         $samples[] = [imagecreatefromjpeg($file)];
-        $labels[] = $piece;
+        $labels[] = $folder;
     }
 }
 
