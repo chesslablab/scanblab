c0dca1c418a6d13b0a612771bfb9aacafedfe325
programarivm <info@programarivm.com>
2023-01-28 16:52:35 +0100

    Prepared empty squares for training

--- cleanup.php
+++ cleanup.php
@@ -1,17 +1,17 @@
 <?php
 
-$pieces = [ 'B', 'b', 'K', 'k', 'N', 'n', 'P', 'p', 'Q', 'q', 'R', 'r', ];
+$folders = [ 'B', 'b', 'K', 'k', 'N', 'n', 'P', 'p', 'Q', 'q', 'R', 'r', 'empty' ];
 
-foreach ($pieces as $piece) {
-    foreach (glob("testing/$piece/*.jpg") as $file) {
+foreach ($folders as $folder) {
+    foreach (glob("testing/$folder/*.jpg") as $file) {
         if (is_file($file)) {
             unlink($file);
         }
     }
 }
 
-foreach ($pieces as $piece) {
-    foreach (glob("training/$piece/*.jpg") as $file) {
+foreach ($folders as $folder) {
+    foreach (glob("training/$folder/*.jpg") as $file) {
         if (is_file($file)) {
             unlink($file);
         }
